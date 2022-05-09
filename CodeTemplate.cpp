bd157455e277f76bdf9829317ce947b2bbbe7577
RIthik Chaudhary <71806874+Rithik57@users.noreply.github.com>
2022-05-09 22:38:38 +0530

    Update CodeTemplate.cpp

--- CodeTemplate.cpp
+++ CodeTemplate.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 
 void init() {
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 200);
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0.0,100.0,0.0,100.0);
-	glutCreateWindow("My Window");
+	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
 }
+
 void display() {
 	glClearColor(0, 0, 0, 0); //black window
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -17,8 +14,13 @@ void display() {
 	
 	glFlush();
 }
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("Main Window");
 	init();
 	glutDisplayFunc(display);
 	glutMainLoop();
